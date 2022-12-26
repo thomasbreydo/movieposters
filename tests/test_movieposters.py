896d1d3d7897c983bcac99eaa0e7c80700ab4174
Thomas Breydo <tbreydo@gmail.com>
2022-12-26 01:33:51 +0000

    Test TV show search too

--- tests/test_movieposters.py
+++ tests/test_movieposters.py
@@ -1,8 +1,12 @@
-import pytest
 import movieposters as mp
 
 
-def test_get_poster():
+def test_get_poster_friends():
+    link = mp.get_poster(title="friends")
+    print(link)
+
+
+def test_get_poster_breakfast_club():
     link = mp.get_poster(title="breakfast club")
     assert link == mp.get_poster(id="tt0088847")
     assert link == mp.get_poster(id=88847)
